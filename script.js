1890052f3a04b087cda3c60c030c069b7e19d6d4
Divyanshu <divyanshukrishna11@gmail.com>
2024-05-14 22:43:36 +0530

    practice-script.js

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-const passwordBox = document.querySelector("input");
+let inp = document.querySelector("input");
 
-const length = 12;
+let length = 12;
 
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
-const lowerCase = "abcdefghijklmnopqrstuvxyz";
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
 
-const number = "0123456789";
+let number = "0123456789";
 
-const symbol = "@#$^()_+|[]></-=";
+let symbol = "!@#$%^&*(){}?/";
 
-const allchars = upperCase + lowerCase + number + symbol;
+let allchars = upperCase + lowerCase + number + symbol;
 
-function createPassword() {
+function createPassword(){
     let password = "";
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    password += number[Math.floor(Math.random() * number.length)];
     password += symbol[Math.floor(Math.random() * symbol.length)];
+    password += number[Math.floor(Math.random() * number.length)];
 
     while(length > password.length){
         password += allchars[Math.floor(Math.random() * symbol.length)];
     }
-    passwordBox.value = password;
+    inp.value = password; 
+
 }
 
 function copyPassword(){
-    passwordBox.select();
+    inp.select();
     document.execCommand("copy");
 }
+
